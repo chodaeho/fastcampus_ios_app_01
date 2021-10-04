2cd52989a7a92db83c9e1d939963316da8478f6f
daehocho <daehocho0637@gmail.com>
2021-10-04 15:06:55 +0900

    아웃렛 변수 및 오토레이아웃의 이해

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 0
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func priceConfirm(_ sender: Any) {
+        let alert = UIAlertController(title: "랜덤 가격표", message: "얼마일까?", preferredStyle: .alert)
+        let action = UIAlertAction(title: "알려주세요.", style: .default, handler: {alert in
+            self.randomPrice()
+        })
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func randomPrice() {
+        let price = arc4random_uniform(10000) + 1
+        currentValue = Int(price)
+        priceLabel.text = "₩\(currentValue)"
+    }
+    
 }
 
